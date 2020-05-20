2802860547e2a97d7bbbc6726644ed7e4c17b294
wallemang98 <wallemang98@gmail.com>
2020-05-19 22:12:16 -0400

    Updates

---
+++ precache-manifest.8bee58f8d29b53bc497c3caaa0bf044f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe6920218ccdba69f8f54fc1948b22f",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "798364714b0b4977b6e2",
+    "url": "/react-todo/static/css/main.1d35b276.chunk.css"
+  },
+  {
+    "revision": "57845a61ae3797cfc893",
+    "url": "/react-todo/static/js/2.1fa62213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.1fa62213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798364714b0b4977b6e2",
+    "url": "/react-todo/static/js/main.f66a1df8.chunk.js"
+  },
+  {
+    "revision": "2294b07f0cda16a9954b",
+    "url": "/react-todo/static/js/runtime-main.dd2745f3.js"
+  }
+]);
