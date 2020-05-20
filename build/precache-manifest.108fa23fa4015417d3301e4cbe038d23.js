d1cda222d96197c0981f4a6a1decf6ddcbfb1745
wallemang98 <wallemang98@gmail.com>
2020-05-19 21:53:44 -0400

    npm run build for web browsers

---
+++ build/precache-manifest.108fa23fa4015417d3301e4cbe038d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b751c07bc15d659b799f3285c75a6971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798364714b0b4977b6e2",
+    "url": "/static/css/main.1d35b276.chunk.css"
+  },
+  {
+    "revision": "57845a61ae3797cfc893",
+    "url": "/static/js/2.1fa62213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fa62213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798364714b0b4977b6e2",
+    "url": "/static/js/main.f66a1df8.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
